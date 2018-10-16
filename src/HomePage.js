c0a0ce10cf2b119f502f7cc6ccce941c946b19b2
Kwabena Boohene <okwabse@gmail.com>
2018-10-16 09:53:57 +0000

    Added icons for about us and home page links

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import { Route, withRouter } from 'react-router-dom';
-import styled from 'styled-components';
-
-import Sidebar from './Sidebar';
-import WelcomePage from './WelcomePage';
-import AboutPage from './AboutPage';
+import React from "react";
+import { Route, withRouter } from "react-router-dom";
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import WelcomePage from "./WelcomePage";
+import AboutPage from "./AboutPage";
+import HomeIcon from "@material-ui/icons/Home";
+import InfoIcon from "@material-ui/icons/Info";
 
 const Wrapper = styled.div`
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
 `;
 
 const PageContent = styled.div`
-    flex: 1;
-    height: 100%;
-    padding: 2%;
+  flex: 1;
+  height: 100%;
+  padding: 2%;
 `;
 
-const HomePage = (props) => (
-    <Wrapper>
-        <Sidebar
-            location={props.location}
-            width={200}
-        >
-            <Sidebar.Entry pathname={'/'} label={'Welcome'} />
-            <Sidebar.Entry pathname={'/about'} label={'About'} />
-        </Sidebar>
-        <PageContent>
-            <Route exact path='/' component={WelcomePage} />
-            <Route path='/about' component={AboutPage} />
-        </PageContent>
-    </Wrapper>
+const HomePage = props => (
+  <Wrapper>
+    <Sidebar location={props.location} width={200}>
+      <Sidebar.Entry pathname={"/"} icon={<HomeIcon />} label={"Welcome"} />
+      <Sidebar.Entry pathname={"/about"} icon={<InfoIcon />} label={"About"} />
+    </Sidebar>
+    <PageContent>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/about" component={AboutPage} />
+    </PageContent>
+  </Wrapper>
 );
 
 export default withRouter(HomePage);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,46 +1,46 @@
 /**
-*
-* Sidebar
-*
-*/
-import React, { Component, createContext } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { LIGHT_GRAY, DARK_GRAY } from './utils/colors';
-import { Link } from 'react-router-dom';
+ *
+ * Sidebar
+ *
+ */
+import React, { Component, createContext } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { LIGHT_GRAY, DARK_GRAY } from "./utils/colors";
+import { Link } from "react-router-dom";
 
 // Styles
 const Wrapper = styled.div`
-  display:flex;
-  flex-direction:column;
-  width: ${(props) => props.width}px;
-  height:100%;
-  background:${LIGHT_GRAY};
+  display: flex;
+  flex-direction: column;
+  width: ${props => props.width}px;
+  height: 100%;
+  background: ${LIGHT_GRAY};
   overflow: auto;
 `;
 
 const ItemWrapper = styled(Link)`
-  height:40px;
-  width:100%;
+  height: 40px;
+  width: 100%;
   padding-left: 20px;
   padding-right: 20px;
   text-decoration: none;
   color: black;
-  ${(props) => props.selected && `background-color: ${DARK_GRAY};`}
+  ${props => props.selected && `background-color: ${DARK_GRAY};`};
 `;
 
 const Item = styled.div`
-  height:100%;
+  height: 100%;
   border-top: 1px solid ${DARK_GRAY};
-  display:flex;
-  align-items:center;
-  flex-direction:row;
-  cursor:pointer;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  cursor: pointer;
 `;
 
-const Icon = styled.img`
-  height:70%;
-  margin-right:15px;
+const Icon = styled.div`
+  height: 70%;
+  margin-right: 15px;
 `;
 
 const Label = styled.span`
@@ -51,7 +51,7 @@ const Label = styled.span`
 `;
 
 const context = createContext({
-  currentPath: '/',
+  currentPath: "/"
 });
 
 const { Provider, Consumer } = context;
@@ -62,14 +62,17 @@ class Entry extends Component {
     const { pathname, icon, label } = this.props;
     return (
       <Consumer>
-        {({ currentPath }) =>
-          (<ItemWrapper to={pathname} selected={pathname.split('/')[1] === currentPath.split('/')[1]}>
+        {({ currentPath }) => (
+          <ItemWrapper
+            to={pathname}
+            selected={pathname.split("/")[1] === currentPath.split("/")[1]}
+          >
             <Item>
-              <Icon src={icon} />
+              <Icon>{icon}</Icon>
               <Label>{label}</Label>
             </Item>
-          </ItemWrapper>)
-        }
+          </ItemWrapper>
+        )}
       </Consumer>
     );
   }
@@ -78,25 +81,22 @@ class Entry extends Component {
 Entry.propTypes = {
   pathname: PropTypes.string,
   icon: PropTypes.string,
-  label: PropTypes.string,
+  label: PropTypes.string
 };
 
 class Sidebar extends Component {
-
   static Entry = Entry;
-  
+
   render() {
     const { location, width } = this.props;
     const pathname = location.pathname;
     return (
       <Provider
         value={{
-          currentPath: pathname,
+          currentPath: pathname
         }}
       >
-          <Wrapper width={width}>
-            {this.props.children}
-          </Wrapper>
+        <Wrapper width={width}>{this.props.children}</Wrapper>
       </Provider>
     );
   }
@@ -106,12 +106,12 @@ Sidebar.propTypes = {
   children: PropTypes.array,
   location: PropTypes.object,
   userData: PropTypes.object,
-  width: PropTypes.number,
+  width: PropTypes.number
 };
 
 Sidebar.defaultProps = {
-    width: 200,
-}
+  width: 200
+};
 
 export default Sidebar;
 /*eslint-enable */
