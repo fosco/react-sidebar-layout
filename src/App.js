01e80d2ecc2ec6abddbf58b6dac5832820af7898
Lorenzo Carducci <card.lorenzo@gmail.com>
2018-10-18 11:11:49 +0200

    Add styled-components for App wrapper div

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import createHistory from 'history/createBrowserHistory';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div style={{ position: 'absolute', width: '100%', height:'100%' }}>
+      <AppWrapper>
         <Router history={createHistory()}>
           <Switch>
             <Route component={HomePage} />
           </Switch>
         </Router>
-      </div>
+      </AppWrapper>
     );
   }
 }
